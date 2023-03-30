a43d140cd82c76aa10ab04d72bc0247d4688b0b4
shashwat-01 <91798934+shashwat-01@users.noreply.github.com>
2023-03-30 11:11:07 +0530

    third-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost'
 import Login from './pages/Login'
